da3fdb2974e4f4d2e05b3aedae1d857d651542d6
salah <salaheddinebaha32@gmail.com>
2026-03-19 02:27:06 +0000

    Added Login Ui Screen And Dash Screen

--- mobile/App.js
+++ mobile/App.js
@@ -1,9 +1,26 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-
+import Login from './Login.js'
+import Dash from './Dash.js';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+const Stack = createNativeStackNavigator();
 export default function App() {
   return (
-    <Text>Bonjour </Text>
+      <NavigationContainer>
+            <Stack.Navigator initialRouteName="Login">
+                <Stack.Screen
+                    name="Login"
+                    component={Login}
+                    options={{ headerShown: false }}
+                />
+                <Stack.Screen
+                    name="Dash"
+                    component={Dash}
+                    options={{ title: 'Tableau de bord' }}
+                />
+            </Stack.Navigator>
+        </NavigationContainer>
   );
 }
 
