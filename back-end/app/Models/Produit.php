2f3897b4aade19fcfcd4f585c2dd94dfefd2e17f
moaaadl <moadlobi@gmail.com>
2026-03-20 23:14:20 +0000

    fix: correct formatting and spacing in Produit model

--- back-end/app/Models/Produit.php
+++ back-end/app/Models/Produit.php
@@ -1,17 +1,19 @@
 <?php
 
 namespace App\Models;
+
 use Illuminate\Database\Eloquent\Attributes\Fillable;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+
 #[Fillable([
-        'libelle',
-        'description',
-        'prix_unitaire',
-        'tva',
-        'unite',
-    
+    'libelle',
+    'description',
+    'prix_unitaire',
+    'tva',
+    'unite',
 ])]
 class Produit extends Model
 {
-     
+    use HasFactory;
 }
