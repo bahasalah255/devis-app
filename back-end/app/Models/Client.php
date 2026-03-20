5e205c6021aa2595b98f779f40936dc18ebd6efc
moaaadl <moadlobi@gmail.com>
2026-03-20 23:14:15 +0000

    fix: add HasFactory trait to Client model for factory support

--- back-end/app/Models/Client.php
+++ back-end/app/Models/Client.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Attributes\Fillable;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 #[Fillable([
@@ -10,10 +11,11 @@ use Illuminate\Database\Eloquent\Model;
     'email',
     'telephone',
     'adresse',
-    
 ])]
 class Client extends Model
 {
+    use HasFactory;
+
     public function devis()
     {
         return $this->hasMany(Devis::class);
