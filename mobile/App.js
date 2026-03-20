f7328697ae599148e3c027b69b245d9591fce39e
moaaadl <moadlobi@gmail.com>
2026-03-20 23:13:40 +0000

    refactor: streamline navigation setup and add UpdateDevis screen

--- mobile/App.js
+++ mobile/App.js
@@ -1,29 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import Login from './Login.js'
+import Login from './Login.js';
 import Dash from './Dash.js';
 import Create from './Create.js';
+import Update from './Update.js';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
 const Stack = createNativeStackNavigator();
+
 export default function App() {
   return (
-      <NavigationContainer>
-            <Stack.Navigator initialRouteName="Login">
-                <Stack.Screen
-                    name="Login"
-                    component={Login}
-                    options={{ headerShown: false }}
-                />
-                <Stack.Screen
-                    name="Dash"
-                    component={Dash}
-                    options={{ title: 'Tableau de bord' }}
-                />
-                 <Stack.Screen name="CreateDevis" component={Create} options={{ title: 'Créer un devis' }} />
-            </Stack.Navigator>
-        </NavigationContainer>
+    <NavigationContainer>
+      <StatusBar style="auto" />
+      <Stack.Navigator initialRouteName="Login" screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="Login"       component={Login} />
+        <Stack.Screen name="Dash"        component={Dash} />
+        <Stack.Screen name="CreateDevis" component={Create} />
+        <Stack.Screen name="UpdateDevis" component={Update} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-
