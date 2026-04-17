6cb47d4c2b708accf1193dfd8e3111551c775977
salah <salaheddinebaha32@gmail.com>
2026-04-17 09:49:22 +0100

    feat: Add trial end date casting and middleware alias for trial management

--- back-end/bootstrap/app.php
+++ back-end/bootstrap/app.php
@@ -9,7 +9,7 @@ use Illuminate\Http\Request;
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
-         api: __DIR__.'/../routes/api.php',
+        api: __DIR__.'/../routes/api.php',
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
@@ -17,6 +17,10 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->redirectGuestsTo(function (Request $request) {
             return $request->is('api/*') ? null : '/login';
         });
+
+        $middleware->alias([
+            'trial' => \App\Http\Middleware\CheckTrial::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         $exceptions->render(function (AuthenticationException $e, Request $request) {
